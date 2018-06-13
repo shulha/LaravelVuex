52baaa234299a330bf8821615cade0e271d117ff
Tkach Aleksandr <o.tkach@linet.sumdu.edu.ua>
2018-06-13 16:40:01 +0300

    feature (Authentication) start

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Authentication</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        Laravel 5 Vue SPA Authentication
                     </div>
                 </div>
             </div>
