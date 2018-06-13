3b69ad120180c105f44be6af1c28f66a40fc03ac
Tkach Aleksandr <o.tkach@linet.sumdu.edu.ua>
2018-06-13 16:08:48 +0300

    feature (NavbarTop.vue) add header in layout

--- resources/assets/js/pages/App.vue
+++ resources/assets/js/pages/App.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <navbar-top></navbar-top>
         <router-view></router-view>
     </div>
 </template>
+
+<script>
+    import NavbarTop from '../components/NavbarTop.vue';
+
+    export default {
+        components: {
+            NavbarTop,
+        },
+    };
+</script>
