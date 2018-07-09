368481f66966cc19cf0221fd7fd60b735c37649d
AleksandrTkach <tkach.aleksandr.s@gmail.com>
2018-07-09 11:01:40 +0300

    fix (SpaController) view('Home') -> view('home')

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SpaController extends Controller
 {
     public function index() {
-        return view('Home');
+        return view('home');
     }
 }
